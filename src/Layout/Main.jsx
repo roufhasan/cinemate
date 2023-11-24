ad6bb9071695cf2691a29f6dfeaedac4d38ce28c
Rouf Hasan Hridoy <roufhasan5@gmail.com>
2023-11-24 23:53:54 +0600

    scroll to top support

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, ScrollRestoration } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
 
 const Main = () => {
   return (
     <div className="max-w-[1920px] mx-auto dark:bg-slate-800">
+      <ScrollRestoration />
       <div className="min-h-[95vh]">
         <Navbar />
         <div className="max-w-7xl mx-auto p-4">
